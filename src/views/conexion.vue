8188f5b2633ef6abd8c120e3b7f39bf8c1837c48
MeldryckSAID <meldryck.said@edu.univ-fcomte.fr>
2023-01-12 20:30:51 +0100

    contenu page co défaillante

--- src/views/conexion.vue
+++ src/views/conexion.vue
@@ -2,64 +2,65 @@
 import f from "/src/components/f.vue";
 import Boutonb from "../components/bouton/boutonb.vue";
 
-import {getAuth, signInWithEmailAndPassword, signOut, } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-auth.js";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut,
+} from "https://www.gstatic.com/firebasejs/9.7.0/firebase-auth.js";
 
-
-export default{
-    data(){
-        return {
-            user:{
-                email:null,
-                password
-            },
-            message:null
-        }
+export default {
+  data() {
+    return {
+      user: {
+        email: null,
+        password,
+      },
+      message: null,
+    };
+  },
+  mounted() {
+    let user = getAuth().currentUser;
+    if (user) {
+      this.user = user;
+      this.message = "user deja co :" + this.user.email;
+    } else {
+      this.message = "user non co ;" + this.user.email;
+    }
+  },
+  methods: {
+    onCnx() {
+      //se connecter avec un mots de pass
+      signInWithEmailAndPassword(getAuth(), this.user.email, this.user.password)
+        .then((response) => {
+          console.log("user connect", response.user);
+          this.user = response.user;
+          this.message = "user connecter:" + this.user.email;
+        })
+        .catch((error) => {
+          //erreur co
+          console.log("erreur connect", error);
+          this.message = "Erreur deco ";
+        });
     },
-    mounted(){
-        let user = getAuth().currentUser;
-        if(user){
-            this.user=user;
-            this.message= "user deja co :" + this.user.email;
-        }else{
-            this.message="user non co ;"+this.user.email
-        }
+
+    onDcnx() {
+      //se deco
+      signOut(getAuth())
+        .then((response) => {
+          this.user = getAuth().currentUser;
+          this.user = {
+            email: null,
+            password: null,
+          };
+          console.log("user  déconnect", this.user);
+          this.message = "user non co ";
+        })
+        .catch((error) => {
+          console.log("erreur  déconnect", error);
+        });
     },
-        methods:{
-            onCnx(){
-                //se connecter avec un mots de pass
-                signInWithEmailAndPassword(getAuth(),this.user.email , this.user.password)
-                .then((response)=>{
-                    console.log('user connect', response.user);
-                    this.user= response.user;
-                    this.message= "user connecter:" +this.user.email
-                })
-                .catch((error)=>{
-                    //erreur co
-                     console.log('erreur connect', error);
-                     this.message = "Erreur deco ";
-                })
-            },
-
-            onDcnx(){
-                //se deco
-                signOut(getAuth())
-                .then(response=>{
-                    this.user = getAuth().currentUser;
-                    this.user= {
-                        email:null,
-                        password:null
-                    };
-                    console.log('user  déconnect', this.user);
-                     this.message = "user non co ";
-
-                })
-                .catch(error=>{
-                      console.log('erreur  déconnect', error);
-                })
-            }
-        }
-    
-}
+  },
+};
 </script>
 
 <template>
@@ -69,36 +70,46 @@ export default{
         <div class="bg-white rounded-lg p-3">
           <!-- champs -->
           <h2 class="text-center okiner text-3xl">Connexion</h2>
-          <div class="relative z-20 flex flex-col w-full">
-            <div class="flex flex-col justify-center m-2">
-              <label class="okiner" for="Mail">Mail </label>
+          <form @submit.prevent="onCnx()">
+            <div class="relative z-20 flex flex-col w-full">
+              <div class="flex flex-col justify-center m-2">
+                <label class="okiner" for="Mail">Mail </label>
 
-              <textarea
-                class="border-2 rounded-2xl border-gray-300 placeholder:text-slate-500 placeholder:okinel"
-                placeholder="Mail"
-                name="Mail"
-                id=""
-                cols="1"
-                rows="1"
-              ></textarea>
+                <textarea
+                  class="border-2 rounded-2xl border-gray-300 placeholder:text-slate-500 placeholder:okinel"
+                  placeholder="Mail"
+                  name="Mail"
+                  v-model="user.email"
+                  required
+                  id=""
+                  cols="1"
+                  rows="1"
+                ></textarea>
+              </div>
+
+              <div class="flex flex-col justify-center mb-4">
+                <label class="okiner" for="Password">Mots de passe </label>
+                <textarea
+                  class="border-2 rounded-2xl border-gray-300 placeholder:text-slate-500 placeholder:okinel"
+                  placeholder="Password"
+                  name="Password"
+                  id=""
+                  v-model="user.password"
+                  required
+                  cols="1"
+                  rows="1"
+                ></textarea>
+              </div>
             </div>
 
-            <div class="flex flex-col justify-center mb-4">
-              <label class="okiner" for="Password">Mots de passe </label>
-              <textarea
-                class="border-2 rounded-2xl border-gray-300 placeholder:text-slate-500 placeholder:okinel"
-                placeholder="Password"
-                name="Password"
-                id=""
-                cols="1"
-                rows="1"
-              ></textarea>
+            <div class="flex justify-center">
+              <boutonb @click="onDcnx" type="submit"> Déconnexion </boutonb>
             </div>
-          </div>
 
-          <div class="flex justify-center">
-            <boutonb> Connexion </boutonb>
-          </div>
+            <div class="flex justify-center">
+              <boutonb type="submit"> Connexion </boutonb>
+            </div>
+          </form>
         </div>
       </div>
       <router-link to="/account">
