91d97ff25bdc4e9903b2a21c5fd64f98aebfb438
MeldryckSAID <meldryck.said@edu.univ-fcomte.fr>
2023-03-11 22:38:16 +0100

    scene de film

--- src/views/index.vue
+++ src/views/index.vue
@@ -207,6 +207,12 @@ window.onload = function () {
                 Ce n'est pas la réalité qui compte dans un film, <br />
                 mais ce que l'imagination peut en faire
               </p>
+               <p class="okiner pb-2 text-left text-lg md:text-xl">
+                Les scènes culte vous connaissez ? </br>
+                Lors de ce défi vous allez devoir </br>
+                reproduire une scène culte de film/série/dessin animée </br>
+                avec un ancien MMI!!!
+              </p>
             </div>
 
             <div class="flex flex-col row-start-1 col-start-3">
